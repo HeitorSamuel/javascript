673d0357bb286d9432894fde6c8849a2325f0457
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-05-04 10:37:16 -0300

    aula12

---
+++ exercicios/aula12/ex012.js
@@ -0,0 +1,13 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são extamente ${hora} horas!`)
+
+if (hora < 5){
+    console.log('Bom Madrugada!')
+} else if (hora < 12 ){
+    console.log('Bom Dia!')
+}else if (hora <= 18){
+    console.log('Boa Tarde!')
+} else {
+    console.log('Boa Noite!')
+}
