31b04f10b2d2b04b836921fe3b4ac48b51362b5c
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-05-06 10:06:41 -0300

    ex016

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,13 @@
+//Repetição com Teste Lógico no Final
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 6)
+
+//Repetição com Teste Lógico no Início
+/*var c = 1 
+while (c <= 10){
+    console.log(`Passo ${c}`)
+    c++
+}*/
