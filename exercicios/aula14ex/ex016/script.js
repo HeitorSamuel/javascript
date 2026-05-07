83926c88ab38de76eac3316c51c4ab78709982ed
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-05-07 11:03:36 -0300

    ex16 final

--- exercicios/aula14ex/ex016/script.js
+++ exercicios/aula14ex/ex016/script.js
@@ -8,10 +8,24 @@ function contar(){
     var passo = Number(txtpasso.value)
     if (inic == "" || fim == ""){
         res.innerHTML = "Impossivel contar!"
-    }else if (passo == '0'){
+    }else if (passo <= 0){
         window.alert('Passo inválido! Considerando PASSO 1!')
+        passo = 1
+    }else{
+        res.innerHTML = '<p>Contando:</p>'
+        if (inic < fim){
+        while (inic <= fim){
+        res.innerHTML += ` ${inic} \u{1F449}`
+        inic+=passo
+        }
+    }else{
+        while (inic >= fim){
+        res.innerHTML += ` ${inic} \u{1F449}`
+        inic-=passo
+        }
     }
-    
+    res.innerHTML += '\u{1F3C1}' 
+    }
 }
 
 
