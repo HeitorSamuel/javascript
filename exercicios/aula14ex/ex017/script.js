0a01ddcbb6160edb3f24742f117e283a569b4ff5
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-05-07 15:48:24 -0300

    ex17

---
+++ exercicios/aula14ex/ex017/script.js
@@ -0,0 +1,20 @@
+function calcular(){
+    var txtnum = document.getElementById('txtnum')
+    var tab = document.getElementById('tab')
+    if (txtnum.value.length == 0){
+        window.alert('Por favor, digite um número!')
+    }else{
+        var num = Number(txtnum.value)
+        var cont = 1
+        tab.innerHTML = ""
+        while (cont <= 10){
+            var option = document.createElement('option')
+            var mult = (num*cont)
+            option.innerHTML = `${num} x ${cont} = ${mult}`
+            cont++
+            tab.appendChild(option)
+            
+        }
+    
+    }
+}
