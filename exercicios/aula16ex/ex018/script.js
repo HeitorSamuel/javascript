a1f2a293abfce76f99138ae172bffeeb90b5c51b
HeitorSamuel <166461014+HeitorSamuel@users.noreply.github.com>
2026-05-10 11:04:33 -0300

    ex018 completo

--- exercicios/aula16ex/ex018/script.js
+++ exercicios/aula16ex/ex018/script.js
@@ -17,6 +17,8 @@ function adicionar(){
                 option.innerHTML += `O Valor ${num} adicionado.`
                 tabadd.appendChild(option)
                 txtnum.value = ''
+                txtnum.focus()
+                res.innerHTML = ''
             }
             
 
@@ -26,9 +28,19 @@ function adicionar(){
 var valores = []
 
 function finalizar(){
-    var res = document.getElementById('res')
-    res.innerHTML = `<p>Ao todo, temos ${valores.length} números cadastrados</p>`
-    res.innerHTML += `<p>O maior valor informado foi ${Math.max(...valores)}</p>` 
-    res.innerHTML += `<p>O menor valor informado foi ${Math.min(...valores)}</p>` 
-    res.innerHTML += `<p>Somando todos os valores, temos ${valores.reduce()}</p>`
+    if (valores.length == 0){
+        window.alert('Adicione valores antes de Finalizar!')
+    }else{
+        var res = document.getElementById('res')
+        res.innerHTML = `<p>Ao todo, temos ${valores.length} números cadastrados</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${Math.max(...valores)}</p>` 
+        res.innerHTML += `<p>O menor valor informado foi ${Math.min(...valores)}</p>` 
+        var soma = 0
+        for (i = 0; i < valores.length; i++){
+            soma+=valores[i]
+        }
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+        media = soma/valores.length
+        res.innerHTML += `A média dos valores digitados é ${media}` 
+        }
 }
